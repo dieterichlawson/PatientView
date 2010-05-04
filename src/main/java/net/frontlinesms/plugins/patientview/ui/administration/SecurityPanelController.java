1e5789af30f04672f50f0ea4387e1b297da75f4f
Walt Javins <walt@javins.net>
2010-05-04 15:18:07 -0700

    Added callback methods for security panel ui.
    Each widget in the security panel ui now appropriately calls a
    method from the controller when a setting is changed.  Furthermore
    when a setting is changed to something outside the allowed range,
    it is automatically reduced to a value in the range.

--- src/main/java/net/frontlinesms/plugins/patientview/ui/administration/SecurityPanelController.java
+++ src/main/java/net/frontlinesms/plugins/patientview/ui/administration/SecurityPanelController.java
@@ -13,14 +13,6 @@ import net.frontlinesms.ui.UiGeneratorController;
 public class SecurityPanelController implements AdministrationTabPanel,
 		ThinletUiEventHandler {
 
-	// Files
-	private final String XML_FILE = "/ui/plugins/patientview/admintab/security_settings.xml";
-
-	// I18N Strings
-	private final String LOCKOUT_DURATION = "admin.security.lockout.duration";
-
-
-
 	/**
 	 * A helper object that keeps track of a specific setting, as well as it's
 	 * minimum and maximum value. The value of the setting can never be set
@@ -40,22 +32,14 @@ public class SecurityPanelController implements AdministrationTabPanel,
 			this.value = value;
 		}
 
-		/** @return the minimum value of this range */
-		public int min() {
-			return min;
-		}
-
 		/** @return the maximum value of this range */
 		public int max() {
 			return max;
 		}
 
-		public int value() {
-			return value;
-		}
-
-		public String textValue() {
-			return "" + value;
+		/** @return the minimum value of this range */
+		public int min() {
+			return min;
 		}
 
 		/**
@@ -79,7 +63,21 @@ public class SecurityPanelController implements AdministrationTabPanel,
 			this.value = value;
 			return true;
 		}
+
+		public String textValue() {
+			return "" + value;
+		}
+
+		public int value() {
+			return value;
+		}
 	}
+
+	// Files
+	private final String XML_FILE = "/ui/plugins/patientview/admintab/security_settings.xml";
+
+	// I18N Strings
+	private final String LOCKOUT_DURATION = "admin.security.lockout.duration";
 
 	// UI Components
 	private final Object mainPanel;
@@ -93,11 +91,13 @@ public class SecurityPanelController implements AdministrationTabPanel,
 	private final Object loginAttemptsBox;
 	private final Object questionAttemptsBox;
 	private final Object lockoutDurationBox;
-	
+
 	// Settings
 	private final Range passwordLength = new Range(6, 12, 7);
 	private final Range requiredQuestions = new Range(1, 4, 3);
-	private final Range requiredAnswers = new Range(1, 4, 2);
+	// cannot be final because the max is dependant on how many questions are
+	// required
+	private Range requiredAnswers = new Range(1, 4, 2);
 	private final Range loginAttempts = new Range(1, 10, 3);
 	private final Range questionAttempts = new Range(1, 10, 3);
 	private final Range lockoutDuration = new Range(1, Integer.MAX_VALUE, 5);
@@ -127,32 +127,128 @@ public class SecurityPanelController implements AdministrationTabPanel,
 		Object label2 = ui.find(mainPanel, "durationlabel2");
 		ui.setText(label1, lockoutText[0].trim());
 		ui.setText(label2, lockoutText[1].trim());
-		//TODO: load settings from file
+		// TODO: load settings from file
 		setDefaultValues();
+	}
+
+	/** Callback method for the lockoutDurationBox. */
+	public void boxChangedLockoutDuration() {
+		spinboxChanged(lockoutDurationBox, lockoutDuration);
+	}
+
+	/** Callback method for the questionAttemptsBox */
+	public void boxChangedLoginAttempts() {
+		spinboxChanged(loginAttemptsBox, loginAttempts);
+	}
+
+	/** Callback method for the passwordLengthBox */
+	public void boxChangedPasswordLength() {
+		spinboxChanged(passwordLengthBox, passwordLength);
+	}
+
+	/** Callback method for the questionAttemptsBox */
+	public void boxChangedQuestionAttempts() {
+		spinboxChanged(questionAttemptsBox, questionAttempts);
+	}
+
+	/** Callback method for the requiredAnswersBox */
+	public void boxChangedRequiredAnswers() {
+		spinboxChanged(requiredAnswersBox, requiredAnswers);
+	}
+
+	/** Callback method for the requiredQuestionsBox */
+	public void boxChangedRequiredQuestions() {
+		spinboxChanged(requiredQuestionsBox, requiredQuestions);
+		// reduce required answers to less than required questions
+		int oldValue = requiredAnswers.value();
+		if (oldValue > requiredQuestions.value()) {
+			oldValue = requiredQuestions.value();
+		}
+		requiredAnswers = new Range(1, requiredQuestions.value(), oldValue);
+		setSpinboxRange(requiredAnswersBox, requiredAnswers);
+
+	}
+
+	/** Callback method for the lowercaseCheckBox */
+	public void boxChangedLowercase() {
+		lowercaseLettersRequired = ui.getBoolean(lowercaseCheckBox, "selected");
+	}
+
+	/** Callback method for the uppercaseCheckBox */
+	public void boxChangedUppercase() {
+		uppercaseLettersRequired = ui.getBoolean(uppercaseCheckBox, "selected");
+	}
+
+	/** Callback method for the numbersCheckBox */
+	public void boxChangedNumbers() {
+		numbersRequired = ui.getBoolean(numbersCheckBox, "selected");
+	}
+
+	public void boxChangedSymbols() {
+		symbolsRequired = ui.getBoolean(symbolsCheckBox, "selected");
+	}
+
+	/* from interface */
+	public String getListItemTitle() {
+		return getI18NString("admin.actionlist.manage.security");
+	}
+
+	/* from interface */
+	public Object getPanel() {
+		return mainPanel;
 	}
 
 	/**
 	 * Sets all of the components in the panel to be their default value.
 	 */
 	protected void setDefaultValues() {
-		ui.setText(passwordLengthBox, passwordLength.textValue());
-		ui.setText(requiredQuestionsBox, requiredQuestions.textValue());
-		ui.setText(requiredAnswersBox, requiredAnswers.textValue());
-		ui.setText(loginAttemptsBox, loginAttempts.textValue());
-		ui.setText(questionAttemptsBox, questionAttempts.textValue());
-		ui.setText(lockoutDurationBox, lockoutDuration.textValue());
+		setSpinboxRange(passwordLengthBox, passwordLength);
+		setSpinboxRange(requiredQuestionsBox, requiredQuestions);
+		setSpinboxRange(requiredAnswersBox, requiredAnswers);
+		setSpinboxRange(loginAttemptsBox, loginAttempts);
+		setSpinboxRange(questionAttemptsBox, questionAttempts);
+		setSpinboxRange(lockoutDurationBox, lockoutDuration);
 		ui.setSelected(lowercaseCheckBox, lowercaseLettersRequired);
 		ui.setSelected(uppercaseCheckBox, uppercaseLettersRequired);
 		ui.setSelected(numbersCheckBox, numbersRequired);
 		ui.setSelected(symbolsCheckBox, symbolsRequired);
 	}
 
-	public String getListItemTitle() {
-		return getI18NString("admin.actionlist.manage.security");
+	/**
+	 * Sets the maximum, minimum, and default values of a spinbox based on a
+	 * range.
+	 * 
+	 * @param spinbox
+	 *            the spinbox to set
+	 * @param range
+	 *            the range of allowable values
+	 */
+	protected void setSpinboxRange(Object spinbox, Range range) {
+		ui.setText(spinbox, range.textValue());
+		ui.setInteger(spinbox, "maximum", range.max());
+		ui.setInteger(spinbox, "minimum", range.min());
 	}
 
-	public Object getPanel() {
-		return mainPanel;
+	/**
+	 * A general handler for a spinbox with a range. This handler gets the new
+	 * value from the spinbox, sets the range to that value, and if the value is
+	 * outside the range, sets the spinbox back to an allowable state (the
+	 * maximum or minimum value).
+	 * 
+	 * @param spinbox
+	 *            the spinbox that changed
+	 * @param range
+	 *            the range associated with the spinbox
+	 */
+	protected void spinboxChanged(Object spinbox, Range range) {
+		String newText = ui.getText(spinbox);
+		if (newText != null && !newText.equals("")) {
+			int newValue = Integer.parseInt(newText);
+			boolean outside = !range.setValue(newValue);
+			if (outside) {
+				ui.setText(spinbox, range.textValue());
+			}
+		}
 	}
 
 }
