a80d3f497949be1e8efc2d070d9d4c8c0cd30db5
Walt Javins <walt@javins.net>
2010-05-05 20:04:55 -0700

    Added reset button functionality.
    When editing a user there is now an option to reset passwords.
    This is set up complete with a .xml widget for the notice, I18N
    strings and all appropriate callbacks.

--- src/main/java/net/frontlinesms/plugins/patientview/ui/administration/UserAdministrationPanelController.java
+++ src/main/java/net/frontlinesms/plugins/patientview/ui/administration/UserAdministrationPanelController.java
@@ -1,5 +1,6 @@
 package net.frontlinesms.plugins.patientview.ui.administration;
 
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,42 +12,107 @@ import net.frontlinesms.plugins.patientview.ui.personpanel.UserPanel;
 import net.frontlinesms.ui.UiGeneratorController;
 
 import org.springframework.context.ApplicationContext;
+
 import static net.frontlinesms.ui.i18n.InternationalisationUtils.*;
 
-public class UserAdministrationPanelController extends PersonAdministrationPanelController<User>{
+/**
+ * The controller for the user administration panel. The associated Thinlet
+ * files are:
+ *<p>
+ *<code>search_action_panel.xml</code><br/>
+ *<code>password_reset_notice.xml</code>
+ */
+public class UserAdministrationPanelController extends
+		PersonAdministrationPanelController<User> {
 
+	/**
+	 * The path to the .xml widget that appears when a user's password is reset.
+	 */
+	private String XML_PASSWORD_RESET_NOTICE = "/ui/plugins/patientview/admintab/password_reset_notice.xml";
+
+	/**
+	 * The notice that appears when a uses's password is reset. This is
+	 * persistant so that multiple copies do not show up if the button is
+	 * clicked multiple times in a row.
+	 */
+	private Object resetNotice;
+
+	/** The userDao for saving users edited in this component */
 	private UserDao userDao;
-	
+
 	public UserAdministrationPanelController(
 			UiGeneratorController uiController, ApplicationContext appCon) {
 		super(uiController, appCon);
+		uiController.setColumns(uiController.find(getPanel(), FIELDS_PANEL), 1);
 	}
 
 	@Override
 	protected List<User> getPeopleForString(String s) {
-		if(userDao == null){
+		if (userDao == null) {
 			userDao = (UserDao) appCon.getBean("UserDao");
 		}
-		return new ArrayList<User>(userDao.getUsersByName(s,-1));
+		return new ArrayList<User>(userDao.getUsersByName(s, -1));
 	}
 
 	@Override
 	protected String getPersonType() {
-		return "User";
+		return getI18NString("medic.common.user");
 	}
 
 	@Override
 	protected void putHeader() {
-		advancedTableController.putHeader(User.class, new String[]{getI18NString("medic.common.labels.name"),getI18NString("medic.common.labels.age"),getI18NString("medic.common.labels.role")}, new String[]{"getName", "getStringAge","getRoleName"});
+		String[] headerText = new String[] {
+				getI18NString("medic.common.labels.name"),
+				getI18NString("medic.common.labels.username"),
+				getI18NString("medic.common.labels.role") };
+		String[] headerMethods = new String[] { "getName", "getUsername",
+				"getRoleName" };
+		advancedTableController
+				.putHeader(User.class, headerText, headerMethods);
 	}
 
 	public String getListItemTitle() {
 		return "Manage Users";
 	}
 
+	/**
+	 * The callback method for the resetPasswordButton.
+	 * 
+	 * @throws GeneralSecurityException
+	 *             if the crypto library cannot be loaded
+	 */
+	public void resetPassword() throws GeneralSecurityException {
+		// TODO: error handling
+		User user = (User) advancedTableController.getCurrentlySelectedObject();
+		String newPass = user.assignTempPassword();
+		userDao.updateUser(user);
+		if (resetNotice == null) {
+			resetNotice = uiController
+					.loadComponentFromFile(XML_PASSWORD_RESET_NOTICE);
+		}
+		Object passwordLabel = uiController.find(resetNotice, "passwordlabel");
+		uiController.setText(passwordLabel, newPass);
+		Object fieldsPanel = uiController.find(getPanel(), FIELDS_PANEL);
+		uiController.add(fieldsPanel, resetNotice);
+	}
+
 	@Override
-	protected PersonPanel getPersonPanelForPerson(Person person) {
-		return new UserPanel(uiController,appCon,(User) person);
+	protected PersonPanel<User> getPersonPanelForPerson(Person person) {
+		return new UserPanel(uiController, appCon, (User) person);
+	}
+
+	@Override
+	public void editButtonClicked() {
+		currentPersonPanel.switchToEditingPanel();
+		// add the reset password button!
+		Object resetPasswordButton = uiController
+				.createButton(getI18NString("password.reset"));
+		uiController.setAction(resetPasswordButton, "resetPassword()", null,
+				this);
+		uiController.setHAlign(resetPasswordButton, "right");
+		Object buttonsPanel = uiController.find(currentPersonPanel
+				.getMainPanel(), PersonPanel.BUTTON_PANEL);
+		uiController.add(buttonsPanel, resetPasswordButton);
 	}
 
 }
