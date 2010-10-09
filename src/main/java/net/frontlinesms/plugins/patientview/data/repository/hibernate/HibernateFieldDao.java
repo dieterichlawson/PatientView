6c5e71a4154ddec3722901e3c3bd4f67c7d250c7
Dieterich Lawson <dieterich.lawson@gmail.com>
2010-10-09 14:51:07 -0500

    Cleaned up FieldDao, adding comments and such

--- src/main/java/net/frontlinesms/plugins/patientview/data/repository/hibernate/HibernateFieldDao.java
+++ src/main/java/net/frontlinesms/plugins/patientview/data/repository/hibernate/HibernateFieldDao.java
@@ -7,6 +7,7 @@ import net.frontlinesms.plugins.patientview.data.domain.framework.Field;
 import net.frontlinesms.plugins.patientview.data.repository.FieldDao;
 
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 
 public class HibernateFieldDao extends BaseHibernateDao<Field> implements FieldDao {
@@ -20,19 +21,19 @@ public class HibernateFieldDao extends BaseHibernateDao<Field> implements FieldD
 	}
 
 	public Field getFieldById(long id) {
-		DetachedCriteria c = super.getCriterion();
+		DetachedCriteria c = DetachedCriteria.forClass(Field.class);
 		c.add(Restrictions.eq("fid", id));
 		return super.getUnique(c);
 	}
 
-	public List<Field> findFieldsByLabel(String label) {
-		DetachedCriteria c = super.getCriterion();
-		c.add(Restrictions.like("label", "%"+label+"%"));
+	public List<Field> findFieldsByLabel(String labelFragment) {
+		DetachedCriteria c = DetachedCriteria.forClass(Field.class);
+		c.add(Restrictions.like("label", labelFragment,MatchMode.ANYWHERE));
 		return super.getList(c);
 	}
 	
 	public List<Field> getFieldsByLabel(String label) {
-		DetachedCriteria c = super.getCriterion();
+		DetachedCriteria c = DetachedCriteria.forClass(Field.class);
 		c.add(Restrictions.eq("label", label));
 		return super.getList(c);
 	}
